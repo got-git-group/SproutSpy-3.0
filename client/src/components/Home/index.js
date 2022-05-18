e8456acce7f428c262644b0d2307a6d64e7f5d81
Andrew An <andyan7@gmail.com>
2022-05-17 21:10:23 -0700

    replication of obsolete repos code

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,22 @@
+// placeholder for future implementation
+import Header from '../Header';
+import Nav from '../Nav';
+import Calendar from '../Calendar';
+import Plants from '../Plants';
+import Footer from '../Footer';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Nav />
+            <Calendar />
+            <Plants />
+            <Footer />
+        </>
+    )
+}
+
+
+
+export default Home;
