731580341f8eacc738f62177db5fa28479ea556b
Samuli Jarvinen <frye@github.com>
2022-05-21 18:14:19 -0700

    Trying to get apollo running

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
-import Header from '../Header';
-import Footer from '../Footer';
 import LoginButton from '../LoginButton';
-import bg from '../../assets/images/projectPhoto.jpg'
-=======
-import bg from '../../assets/images/projectphoto.jpg';
+import bg from '../../assets/images/projectPhoto.jpg';
 import './index.scss'
->>>>>>> main
 
 const Home = () => {
     return (
         <>
             <img src={bg} alt='' className='image'>
             </img>
-<<<<<<< HEAD
             <LoginButton />
-            <Footer />
-=======
->>>>>>> main
         </>
     )
 }
