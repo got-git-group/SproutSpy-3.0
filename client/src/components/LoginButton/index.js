64ea92b8d961ac7ba508c924fb13c2e4965f0f12
Andrew An <andyan7@gmail.com>
2022-05-21 20:06:55 -0700

    styled buttons

--- client/src/components/LoginButton/index.js
+++ client/src/components/LoginButton/index.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return(
-    <button className='buttonlog' onClick={() => loginWithRedirect()}>
+    <button onClick={() => loginWithRedirect()}>
       Log In
     </button>
   ); 
