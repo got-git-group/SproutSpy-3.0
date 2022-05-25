382d6957b1243a9a74e1a338af8b3ecb9d1a4443
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-05-24 20:58:03 -0700

    built out results page and query with dummy zoneId, built plant cards to generate based on zoneId, styled

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Home from './components/Home';
 import AddPlant from './components/AddPlant';
 import Glossary from './components/Glossary';
 import MyCalendar from './components/Calendar';
+import Results from './components/Results';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -23,7 +24,8 @@ function App() {
           <Route index element={ <Home /> } />
           <Route path="addplant" element={ <AddPlant /> } />
           <Route path="glossary" element={ <Glossary /> } />
-          <Route path="calendar" element={ <MyCalendar /> } />          
+          <Route path="calendar" element={ <MyCalendar /> } />
+          <Route path="results" element={ <Results /> } />
         </Route>
       </Routes>
     </ApolloProvider>
--- client/src/components/Plants/index.js
+++ client/src/components/Plants/index.js
@@ -1,10 +1,36 @@
 import './index.scss';
+import React from "react";
 
-const Plants = () => {
+const Plants = ( { plants } ) => {
+    console.log(plants);
     return (
-        <>
-            <p>Placeholder</p>
-        </>
+        <div>
+            <h1>Results</h1>
+            <section id="plantFlex">
+                {plants.recommendedPlants.map((plant) => (
+                    <div class="plantCard">
+                        <h2 class="plantName">{plant.plantName}</h2>
+                        <img class="plantImage" src={plant.plantImg} alt={plant.plantName} />
+                        <ul class="plantDetails">
+                            <li>Spacing: {plant.spacing}</li>
+                            <li>Seed Depth: {plant.seedDepth}</li>
+                            <li>Sunlight: {plant.sunlight}</li>
+                        </ul>
+                    </div>
+                ))}
+                {plants.nonRecommendedPlants.map((plant) => (
+                    <div class="plantCard">
+                        <h2 class="plantName">{plant.plantName}</h2>
+                        <img class="plantImage" src={plant.plantImg} alt={plant.plantName} />
+                        <ul class="plantDetails">
+                            <li>Spacing: {plant.spacing}</li>
+                            <li>Seed Depth: {plant.seedDepth}</li>
+                            <li>Sunlight: {plant.sunlight}</li>
+                        </ul>
+                    </div>
+                ))}
+            </section>
+        </div>
     )
 }
 
