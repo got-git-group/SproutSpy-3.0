90307499d2b5b2d4b1d9771ab5a24b348d10009e
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-05-28 15:04:31 -0700

    welcome added to home page with list of zones to choose from, passing zone_id as prop to results page to generate results. small styling changes to glossary

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,12 +1,30 @@
-import bg from '../../assets/images/projectPhoto.jpg';
-import './index.scss'
+import './index.scss';
+import React from "react";
+import { useQuery } from '@apollo/client';
+import { QUERY_ZONES } from '../../utils/queries';
 
 const Home = () => {
+    const { loading, error, data } = useQuery(QUERY_ZONES);
+    console.log(data);
+    if (!data) {
+        return <p>Loading...</p>;
+    }
+    // I THINK AN AWAIT OR SOMETHING IS NEEDED HERE
     return (
-        <>
-            <img src={bg} alt='' className='backgroundImage'>
-            </img>
-        </>
+        <div id="homePage">
+            <form id="zoneSelect">
+                <h1 id="welcome">Welcome!</h1>
+                <h3 id="choose">Choose your grow zone to get started:</h3>
+                <div class="dropdown">
+                    <button class="dropbtn">Zones</button>
+                    <div class="dropdown-content">
+                        {data.zones.map((zone) => (
+                            <a href="/results" {...data}>{zone.zoneName}</a>
+                        ))}
+                    </div>
+                </div>
+            </form>
+        </div>
     )
 }
 
