2f992e42cb6ce4ede4229dcca10f6b14945febb5
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-05-28 09:56:09 -0700

    changing url typo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/sproutspy', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sproutspy', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
