54b7bba7de38923e8ce006e776e31b432b5a7a42
Andrew An <andyan7@gmail.com>
2022-05-28 23:26:32 -0700

    logout button wrapped

--- client/src/components/LogoutButton/index.jsx
+++ client/src/components/LogoutButton/index.jsx
@@ -3,11 +3,17 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
-  return(
-    <button onClick={() => logout()}>
-      Log Out
-    </button>
-  ); 
+  return (
+    <>
+        <div className='wrapKlass'>
+          <button onClick={() => logout()}>
+            <p className='paraKlass'>
+            Log Out
+            </p>
+          </button>
+        </div>
+    </>
+  )
 }
 
 export default LogoutButton;
