a240c201be7909609f5de54d2939d316700a4104
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-05-28 19:25:11 -0700

    add plant form front end built. attempted to get results to generate using zone passed in from home but not working yet

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -5,23 +5,23 @@ import { useQuery } from '@apollo/client';
 import { QUERY_RECOMMENDED_PLANTS } from '../../utils/queries';
 import Plants from '../Plants';
 
-const Results = ( { zones }) => {
+const Results = (zone) => {
     // will need to pass in the zoneID to these queries
-    console.log(zones);
+    console.log(zone);
+
     const { loading, error, data } = useQuery(QUERY_RECOMMENDED_PLANTS, {variables: {zoneId: '629280eaf1afa589544b3656'}});
     console.log(data);
     if (!data) {
         return <p>Loading...</p>;
-    }
+    };
     return (
         <div>
-            {/* loop through results and create a plant card for each result, putting those with the recommended value first */}
             <Plants
             plants = {data}
             />
 
         </div>
     )
-}
+};
 
 export default Results;
