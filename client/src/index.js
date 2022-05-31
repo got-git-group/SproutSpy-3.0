867608de85dfb6b040b6897d659fc0773e1ac9cb
Samuli Jarvinen <frye@github.com>
2022-05-31 15:56:07 -0700

    Testing

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN ;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
+console.log(clientId, domain);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
